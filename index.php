1bcfd435c8f105ebb5e796847b5d57840d858c83
Jahangir Alam Rocky <jahangir64r@gmail.com>
2023-10-17 00:43:12 +0600

    style: Add meta tags to index demo. - Add description in project index.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ Utils::init();
 <html lang="en">
 
 <head>
-    <title>INDEX | jahangir1x</title>
+    <title>repo-man</title>
 
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -23,11 +23,23 @@ Utils::init();
 
     <link id="favicon" rel="icon" href="" />
 
+    <link href=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
 </head>
 
 <body data-bs-theme="dark">
     <div class="container">
-        <h3 class="display-6 text-center py-3">All Repositories</h3>
+        <div class="text-center py-3">
+            <a class="display-6 heading" href="https://github.com/jahangir1x/repo-man" target="_blank">
+                Repository Manager 🛠️
+            </a>
+            <br>
+            <i class="description">A PHP-powered tool to stay up-to-date with remote repositories.</i>
+        </div>
+
         <table class="table table-striped">
             <tr>
                 <th>Path</th>
@@ -37,7 +49,7 @@ Utils::init();
     </div>
     <?php foreach (Utils::getAllRepoFolders() as $folder): ?>
         <tr id=<?= $folder ?>>
-            <td class="path-url"><a href="<?= "../" . basename($folder) ?>">
+            <td class="path-url"><a href="<?= "../" . basename($folder) ?>" target="_blank">
                     <?= basename($folder) ?>
                 </a>
                 <div class="spinner"></div>
@@ -55,7 +67,7 @@ Utils::init();
                 </form>
             </td>
             <td>
-                <a href="<?= Git::getRemoteUrl($folder) ?>">
+                <a href="<?= Git::getRemoteUrl($folder) ?>" target="_blank">
                     <?= Git::getRemoteUrl($folder) ?>
                 </a>
             </td>
@@ -63,10 +75,6 @@ Utils::init();
     <?php endforeach; ?>
     </table>
     </div>
-    <link href=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="css/style.css">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
