503a887c6d7d9cb8bf716b2956e5e0008e78eb66
Jahangir Alam Rocky <jahangir64r@gmail.com>
2023-10-16 20:42:54 +0600

    feat: add remote url getter in Git. - Add dark mode in index.

--- Git.php
+++ Git.php
@@ -6,8 +6,7 @@ class Git
     {
 
         if (Utils::isValidDirectory($folder)) {
-            $fullCommand = "git -C " . $folder . " fetch --dry-run; git -C " . $folder . " status -uno" . " 2>&1";
-            $output = shell_exec($fullCommand);
+            $output = shell_exec("git -C " . $folder . " fetch --dry-run; git -C " . $folder . " status -uno" . " 2>&1");
             return strpos($output, 'Your branch is behind') !== false;
         } else {
             return false;
@@ -18,8 +17,17 @@ class Git
     public static function pull($folder): void
     {
         if (Utils::isValidDirectory($folder)) {
-            $fullCommand = "git -C " . $folder . " --rest HEAD; git -C " . $folder . " pull" . " 2>&1";
-            shell_exec($fullCommand);
+            shell_exec("git -C " . $folder . " --rest HEAD; git -C " . $folder . " pull" . " 2>&1");
+        }
+    }
+
+    // Get remote repository URL
+    public static function getRemoteUrl($folder): string
+    {
+        if (Utils::isValidDirectory($folder)) {
+            return shell_exec("git -C " . $folder . " config --get remote.origin.url" . " 2>&1");
+        } else {
+            return "o_O could not find url";
         }
     }
 }
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'Git.php';
 require_once 'Utils.php';
 Utils::init();
 ?>
@@ -6,30 +7,50 @@ Utils::init();
 <html>
 
 <head>
-    <title>Git Update Checker</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    <title>INDEX | jahangir1x</title>
+    <link href=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-<body>
+<body data-bs-theme="dark">
     <div class="container">
-        <?php foreach (Utils::getAllRepoFolders() as $folder): ?>
-            <h2>Folder:
-                <?= $folder ?>
-            </h2>
-            <form class="check-repo-form">
-                <div class="result">
-                </div>
-                <input type='hidden' class='folder' value=<?= $folder ?>>
-                <button type="submit">Check for Updates</button>
-            </form>
-            <form class="update-repo-form">
-                <div class="result">
-                </div>
-                <input type='hidden' class='folder' value=<?= $folder ?>>
-                <button type="submit">Update Repo</button>
-            </form>
-        <?php endforeach; ?>
+        <h3 class="display-6">All Repositories</h3>
+        <table class="table table-striped">
+            <tr>
+                <th>Path</th>
+                <th>Action</th>
+                <th>Repository</th>
+            </tr>
+            <?php foreach (Utils::getAllRepoFolders() as $folder): ?>
+                <tr>
+                    <td><a href="<?= "../" . basename($folder) ?>">
+                            <?= basename($folder) ?>
+                        </a>
+                    </td>
+                    <td>
+                        <div class="action">
+                            <form class="check-repo-form">
+                                <div class="result">
+                                </div>
+                                <input type='hidden' class='folder' value=<?= $folder ?>>
+                                <button type="submit">Check for Updates</button>
+                            </form>
+                        </div>
+                    </td>
+                    <td>
+                        <a href="<?= Git::getRemoteUrl($folder) ?>">
+                            <?= Git::getRemoteUrl($folder) ?>
+                        </a>
+                    </td>
+                    <!-- <form class="update-repo-form">
+                        <div class="result">
+                        </div>
+                        <input type='hidden' class='folder' value=<?= $folder ?>>
+                        <button type="submit">Update Repo</button>
+                    </form> -->
+                </tr>
+            <?php endforeach; ?>
+        </table>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
